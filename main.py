8fb0365e9ddd695d4662080c2122f7340a35f51d
Eric Drechsler <eric.drechsler@1qbit.com>
2022-10-20 23:17:44 +0000

    running benchmarks

--- main.py
+++ main.py
@@ -22,39 +22,50 @@ from ml_collections.config_flags import config_flags
 import logging
 import os
 import tensorflow as tf
+import time
 
 FLAGS = flags.FLAGS
 
 config_flags.DEFINE_config_file(
-  "config", None, "Training configuration.", lock_config=True)
+    "config", None, "Training configuration.", lock_config=True
+)
 flags.DEFINE_string("workdir", None, "Work directory.")
 flags.DEFINE_enum("mode", None, ["train", "eval"], "Running mode: train or eval")
-flags.DEFINE_string("eval_folder", "eval",
-                    "The folder name for storing evaluation results")
+flags.DEFINE_string(
+    "eval_folder", "eval", "The folder name for storing evaluation results"
+)
 flags.mark_flags_as_required(["workdir", "config", "mode"])
 
 
 def main(argv):
-  if FLAGS.mode == "train":
-    # Create the working directory
-    tf.io.gfile.makedirs(FLAGS.workdir)
-    # Set logger so that it outputs to both console and file
-    # Make logging work for both disk and Google Cloud Storage
-    gfile_stream = open(os.path.join(FLAGS.workdir, 'stdout.txt'), 'w')
-    handler = logging.StreamHandler(gfile_stream)
-    formatter = logging.Formatter('%(levelname)s - %(filename)s - %(asctime)s - %(message)s')
-    handler.setFormatter(formatter)
-    logger = logging.getLogger()
-    logger.addHandler(handler)
-    logger.setLevel('INFO')
-    # Run the training pipeline
-    run_lib.train(FLAGS.config, FLAGS.workdir)
-  elif FLAGS.mode == "eval":
-    # Run the evaluation pipeline
-    run_lib.evaluate(FLAGS.config, FLAGS.workdir, FLAGS.eval_folder)
-  else:
-    raise ValueError(f"Mode {FLAGS.mode} not recognized.")
+    start_time = time.time()
+
+    if FLAGS.mode == "train":
+        # Create the working directory
+        tf.io.gfile.makedirs(FLAGS.workdir)
+        # Set logger so that it outputs to both console and file
+        # Make logging work for both disk and Google Cloud Storage
+        gfile_stream = open(os.path.join(FLAGS.workdir, "stdout.txt"), "w")
+        handler = logging.StreamHandler(gfile_stream)
+        formatter = logging.Formatter(
+            "%(levelname)s - %(filename)s - %(asctime)s - %(message)s"
+        )
+        handler.setFormatter(formatter)
+        logger = logging.getLogger()
+        logger.addHandler(handler)
+        logger.setLevel("INFO")
+        # Run the training pipeline
+
+        run_lib.train(FLAGS.config, FLAGS.workdir)
+    elif FLAGS.mode == "eval":
+        # Run the evaluation pipeline
+        run_lib.evaluate(FLAGS.config, FLAGS.workdir, FLAGS.eval_folder)
+    else:
+        raise ValueError(f"Mode {FLAGS.mode} not recognized.")
+    print(f"app runtime {time.time()-start_time}")
 
 
 if __name__ == "__main__":
-  app.run(main)
+    start_time = time.time()
+    app.run(main)
+    print(f"Global runtime {time.time()-start_time}")
